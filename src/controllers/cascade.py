2711a378aa8c8cbe712b768cde6be5db76e00f5d
keith.lewis <keith.lewis@pont-mbale.org.uk>
2011-12-11 20:33:56 +0000

    more cascading

--- src/controllers/cascade.py
+++ src/controllers/cascade.py
@@ -2,32 +2,34 @@
 
 #import widgets
 
-def buildSelect(field, value, wrapper):
-        default = dict(value=value)
-        attr = FormWidget._attributes(field, default)
-        requires = field.requires
-        options = []
-        for (k, v) in requires.options(True):
-            optClass = 'static'
-            if isinstance(v, tuple):
-                optClass = 'sub_' + str(v[0])
-                v = v[1]
-            options.append(OPTION(v, _value=k, _class=optClass))
+def buildSelect(field, wrapper):
+    requires = field.requires
+    options = []
+    for (k, v) in requires.options(True):
+        optClass = 'static'
+        if isinstance(v, tuple):
+            optClass = 'sub_' + str(v[0])
+            v = v[1]
+        options.append(OPTION(v, _value=k, _class=optClass))
 
-        wrapper.append(SELECT(*options, **attr))
+    attr = FormWidget._attributes(field, {})
+    wrapper.append(SELECT(*options, **attr))
 
 class CascadingSelect(FormWidget):
-    def __init__(self, parent):
-        self.parent = parent
+    def __init__(self, *parents):
+        self.parents = parents
         
     def widget(self, field, value, **attributes):
         tableId = '%s_%s_cascade' % (field._tablename, field.name)
         wrapper = TABLE(_id = tableId)
-        buildSelect(field, value, wrapper)
+        for parent in self.parents:
+            buildSelect(parent, wrapper)
+        buildSelect(field, wrapper)
         return wrapper
 
 def index():
-    cascadeWidget=CascadingSelect('parish')
+    cascadeWidget=CascadingSelect(db.subCounty.district, db.parish.subCounty,
+                                  db.village.parish)
     form = SQLFORM.factory(
         Field('village',
               requires=IS_IN_DB(db, 'village.id', db.village._format),
